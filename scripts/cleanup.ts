3c39de2f7e9c57381374c24919ce1ee1fed943ce
Soybean <honghuangdc@gmail.com>
2023-01-10 07:51:02 +0800

    build(projects): update deps and update package.json

--- scripts/cleanup.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zx
-import { $ } from 'zx';
-
-$`pnpm rimraf node_modules packages/**/node_modules dist ./**/dist pnpm-lock.yaml packages/**/pnpm-lock.yaml`;
--- scripts/sync.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zx
-import { fetch } from 'zx';
-import pkg from '../package.json';
-
-const requestUrl = `https://npmmirror.com/sync/${pkg.name}`;
-
-fetch(requestUrl);
